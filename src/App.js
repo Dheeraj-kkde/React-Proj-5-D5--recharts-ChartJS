ded81b9580fbddd5cc7d470123b913a2843b5c80
Dheeraj Kumar <91405138+Dheeraj-kkde@users.noreply.github.com>
2023-11-17 13:24:01 +0530

    implemented the charts using  recharts and chartjs in react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import RechartsExample from "./components/RechartsExample";
+import ChartJsExample from "./components/ChartJsExample";
+
+const App = () => {
+  return (
+    <div>
+      <h1>ReCharts and ChartJS application</h1>
+      <RechartsExample />
+      <ChartJsExample />
+    </div>
+  );
+};
+
+export default App;
